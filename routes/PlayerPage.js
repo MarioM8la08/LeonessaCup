3023275e6ca9dc281e88ba7f5f971f64a5175e39
MarioM8la08 <mario.m8la@gmail.com>
2025-03-03 12:17:56 +0100

    "player date page backend"

--- routes/PlayerPage.js
+++ routes/PlayerPage.js
@@ -5,19 +5,24 @@ router.get('/api/player/Data', async (req, res) => {
     const squadraID = req.query['playerID'];
     try {
         const query = 'SELECT giocatori.id_giocatore,\n' +
-            '       giocatori.nome_cognome,\n' +
-            '       giocatori.anno_scolastico,\n' +
-            '       giocatori.numero_maglia,\n' +
-            '       giocatori.ruolo,\n' +
-            '       SUM(prestazioni.gol) AS total_gol,\n' +
-            '       SUM(prestazioni.assist) AS total_assist,\n' +
-            '       SUM(prestazioni.cartellini_gialli) AS total_cartellini_gialli,\n' +
-            '       SUM(prestazioni.cartellini_rossi) AS total_cartellini_rossi,\n' +
-            '       SUM(prestazioni.minuti_giocati) AS total_minuti_giocati\n' +
-            'FROM giocatori\n' +
-            '         JOIN prestazioni ON giocatori.id_giocatore = prestazioni.id_giocatore\n' +
-            'WHERE giocatori.id_giocatore = $1\n' +
-            'GROUP BY giocatori.id_giocatore, giocatori.nome_cognome, giocatori.anno_scolastico, giocatori.numero_maglia, giocatori.ruolo;';
+            '                     giocatori.id_squadra,\n' +
+            '                     giocatori.nome_cognome,\n' +
+            '                     giocatori.anno_scolastico,\n' +
+            '                     giocatori.numero_maglia,\n' +
+            '                     giocatori.ruolo,\n' +
+            '                     squadre.nome_scuola,\n' +
+            '                     SUM(prestazioni.gol) AS total_gol,\n' +
+            '                     SUM(prestazioni.assist) AS total_assist,\n' +
+            '                     SUM(prestazioni.cartellini_gialli) AS total_cartellini_gialli,\n' +
+            '                     SUM(prestazioni.cartellini_rossi) AS total_cartellini_rossi,\n' +
+            '                     SUM(prestazioni.minuti_giocati) AS total_minuti_giocati,\n' +
+            '                     SUM(CASE WHEN prestazioni.titolare THEN 1 ELSE 0 END) AS total_titolare_true,\n' +
+            '                     SUM(CASE WHEN prestazioni.titolare THEN 0 ELSE 1 END) AS total_titolare_false\n' +
+            '              FROM giocatori\n' +
+            '              JOIN prestazioni ON giocatori.id_giocatore = prestazioni.id_giocatore\n' +
+            '              JOIN squadre ON giocatori.id_squadra = squadre.id_squadra\n' +
+            '              WHERE giocatori.id_giocatore = $1\n' +
+            '              GROUP BY giocatori.id_giocatore, giocatori.nome_cognome, giocatori.anno_scolastico, giocatori.numero_maglia, giocatori.ruolo, squadre.nome_scuola;';
         const result = await pool.query(query, [squadraID]);
         if (result.rows.length === 0) {
             return res.status(404).send('Squadra not found');
