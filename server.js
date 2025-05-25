8908dd4df369be7b64718dcd0d78762cf558052f
MarioM8la08 <mario.m8la@gmail.com>
2025-05-25 13:51:04 +0200

    "fix countdown"

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 
 // Usa la porta assegnata da Azure oppure 3000 in locale
 const port = process.env.PORT || 3000;
-
+app.use(express.json());
 const pool = require('./routes/db.js');
 const dataTeamRouter = require('./routes/DataTeam.js');
 const dataGironiRouter = require('./routes/ClassificaGironi.js');
@@ -18,6 +18,8 @@ const ytMatchs = require('./routes/ytMatchs.js');
 const ytMatch = require('./routes/ytMatch.js');
 const MatchStat = require('./routes/MatchStat.js');
 const eventiMatch = require('./routes/eventiMatch.js');
+const adminReg = require('./routes/admin.js');
+const modificaClassifica = require('./routes/modificaClassifica.js');
 
 app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
@@ -32,13 +34,18 @@ app.use(ytMatchs);
 app.use(ytMatch);
 app.use(MatchStat);
 app.use(eventiMatch);
-
+app.use(adminReg);
+app.use(modificaClassifica);
 // Gestione richieste a file HTML direttamente
 app.get('/*.html', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', req.path));
 });
 app.get('/', (req, res) => {
     res.redirect('/chiSiamo');
+});
+// Gestione singola rout inserimento
+app.get('/inserimenti', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'inserimenti.html'));
 });
 // Per tutte le altre rotte, invia index.html (SPA fallback)
 app.get('*', (req, res) => {
