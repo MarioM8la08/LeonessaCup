9e36710eefcca702d76c64d15da9a5debfe3d225
MarioM8la08 <mario.m8la@gmail.com>
2025-05-26 18:06:13 +0200

    "data fix"

--- public/js/client.js
+++ public/js/client.js
@@ -500,7 +500,7 @@ async function initMatchSeason() {
             slider.innerHTML += `                
                 <div id="giornata${i}" class="giornata">
                     <div class="hGiornate">
-                        <h1 id="finestraTempGirone${i}" class="titleGiornate">${Snododata(dataPartite[0]["data_ora"])}</h1>
+                        <h1 id="finestraTempGirone${i}" class="titleGiornate">${Snododata(dataPartite[2]["data_ora"])}</h1>
                         <h4 class="nGiornate">Giornata ${i}</h4>
                     </div>
                 </div>`;
