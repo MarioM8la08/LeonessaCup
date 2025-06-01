86609c10ccc2415bb3e834f261b2c77f09212348
MarioM8la08 <mario.m8la@gmail.com>
2025-06-01 17:02:39 +0200

    "credential mailgun"

--- routes/sendEmail.js
+++ routes/sendEmail.js
@@ -1,14 +1,15 @@
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-    host: 'smtp.office365.com',
+    host: 'smtp.mailgun.org',
     port: 587,
     secure: false,
     auth: {
-        user: "71398061@itiscastelli.onmicrosoft.com",
-        pass: "NiVero3",
+        user: "mariom8la@leonessacup.live",
+        pass: "ab870d50cd0df47fedb41e81f732cea4-7c5e3295-26ff23a8",
     }
 });
+
 async function sendEmail(to, subject, content, isHtml = false, attachments = []) {
     // Normalizza attachments
     const normalizedAttachments = Array.isArray(attachments)
