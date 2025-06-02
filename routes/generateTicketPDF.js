214c15338f1637c2456345199b98b4c0a73bde18
MarioM8la08 <mario.m8la@gmail.com>
2025-06-02 14:05:16 +0200

    "pdf fix"

--- routes/generateTicketPDF.js
+++ routes/generateTicketPDF.js
@@ -2,16 +2,19 @@ const puppeteer = require('puppeteer');
 
 async function generateTicketPDF(html) {
     const browser = await puppeteer.launch({
-        headless: 'new',
+        headless: true,
         args: ['--no-sandbox', '--disable-setuid-sandbox']
     });
     const page = await browser.newPage();
     await page.setContent(html, { waitUntil: 'networkidle0' });
+
+    console.log('Generating PDF...');
     const pdfBuffer = await page.pdf({
-        width: '80mm',
         height: '40mm',
         printBackground: true
     });
+    console.log('PDF generated, is Buffer:', Buffer.isBuffer(pdfBuffer));
+
     await browser.close();
     return pdfBuffer;
 }
