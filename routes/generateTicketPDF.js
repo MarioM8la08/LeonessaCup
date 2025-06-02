ffa382ea4779160cade4f2665085e47d99127240
MarioM8la08 <mario.m8la@gmail.com>
2025-06-02 12:52:33 +0200

    "credential"

--- routes/generateTicketPDF.js
+++ routes/generateTicketPDF.js
@@ -1,7 +1,10 @@
 const puppeteer = require('puppeteer');
 
 async function generateTicketPDF(html) {
-    const browser = await puppeteer.launch({ headless: 'new' });
+    const browser = await puppeteer.launch({
+        headless: 'new',
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
+    });
     const page = await browser.newPage();
     await page.setContent(html, { waitUntil: 'networkidle0' });
     const pdfBuffer = await page.pdf({
