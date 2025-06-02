c427c8f05e16b079d34a5eff3b55e6ca5906ac49
MarioM8la08 <mario.m8la@gmail.com>
2025-06-02 12:04:16 +0200

    "credential"

--- routes/sendEmail.js
+++ routes/sendEmail.js
@@ -1,30 +1,33 @@
-const nodemailer = require('nodemailer');
+const FormData = require("form-data");
+const Mailgun = require("mailgun.js");
 
-const transporter = nodemailer.createTransport({
-    host: 'smtp.mailgun.org',
-    port: 587,
-    secure: false,
-    auth: {
-        user: "mariom8la@leonessacup.live",
-        pass: "d30ad1df4704be0b0ad4022fa2dd7e8c-08c79601-72bf022a",
-    }
+const mailgun = new Mailgun(FormData);
+const mg = mailgun.client({
+    username: "api",
+    key: "02984bee26dcda63516b21dd5a406c4c-08c79601-8853a447", // <-- Inserisci qui la tua vera API key di Mailgun
+    url: "https://api.eu.mailgun.net"
 });
 
 async function sendEmail(to, subject, content, isHtml = false, attachments = []) {
-    // Normalizza attachments
-    const normalizedAttachments = Array.isArray(attachments)
-        ? attachments
-        : [attachments];
+    try {
+        const message = {
+            from: 'Leonessa Cup <postmaster@sandbox707552b8698c48218c06c1b1911e19c3.mailgun.org>',
+            to: [to],
+            subject,
+            [isHtml ? 'html' : 'text']: content,
+        };
 
-    const mailOptions = {
-        from: 'Leonessa Cup <mariom8la@leonessacup.live>',
-        to,
-        subject,
-        [isHtml ? 'html' : 'text']: content,
-        attachments: normalizedAttachments
-    };
+        if (attachments.length > 0) {
+            message.attachment = attachments;
+        }
 
-    return transporter.sendMail(mailOptions);
+        const result = await mg.messages.create("sandbox707552b8698c48218c06c1b1911e19c3.mailgun.org", message);
+        console.log("Email inviata:", result);
+        return result;
+    } catch (error) {
+        console.error("Errore durante l'invio:", error);
+        throw error;
+    }
 }
 
 module.exports = sendEmail;
